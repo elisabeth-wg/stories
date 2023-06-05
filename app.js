d7909ab0848fe7888299c07afc07a9c1f4d5612d
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-06-05 17:47:25 -0500

    Almost complete

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
@@ -19,14 +20,43 @@ connectDB()
 
 const app = express()
 
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
 // Logging
 if(process.env.Node_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+// Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
+
 // Handlebars
 // !Add the word .engine after exphbs as below
-app.engine('.hbs', exphbs.engine({
+app.engine(
+  '.hbs',
+  exphbs.engine({
+    helpers: {
+        formatDate,
+        stripTags,
+        truncate,
+        editIcon,
+        select,
+    },
     defaultLayout: 'main',
     extname: '.hbs'
     })
@@ -49,6 +79,13 @@ app.use(
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+
+// Set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
